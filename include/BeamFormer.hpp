3ebcd79f62d6e58012e6c156c8b5c01ef33c9257
Alessio Sclocco <alessio@sclocco.eu>
2014-04-01 20:58:17 -0700

    New license: Apache 2.0.

--- include/BeamFormer.hpp
+++ include/BeamFormer.hpp
@@ -1,20 +1,16 @@
+// Copyright 2011 Alessio Sclocco <a.sclocco@vu.nl>
 //
-// Copyright (C) 2011
-// Alessio Sclocco <a.sclocco@vu.nl>
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
 //
-// This program is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
+//     http://www.apache.org/licenses/LICENSE-2.0
 //
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with this program.  If not, see <http://www.gnu.org/licenses/>.
-//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 #include <string>
 using std::string;
@@ -141,15 +137,15 @@ template< typename T > void BeamFormer< T >::generateCode() throw (OpenCLError) 
 	"<%STORE%>"
 	"}\n"
 	"}\n";
-	
+
 	string definitionsTemplate = Kernel< T >::getDataType() + "4 beam<%NUM%> = (" + Kernel< T >::getDataType() + "4)(0.0f);\n";
-	
+
 	string beamsTemplate = "weight = weights[item++];\n"
 	"beam<%NUM%>.x += (cSample.x * weight.x) - (cSample.y * weight.y);\n"
 	"beam<%NUM%>.y += (cSample.x * weight.y) + (cSample.y * weight.x);\n"
 	"beam<%NUM%>.z += (cSample.z * weight.x) - (cSample.w * weight.y);\n"
 	"beam<%NUM%>.w += (cSample.z * weight.y) + (cSample.w * weight.x);\n";
-	
+
 	string averageTemplate = "beam<%NUM%> *= " + averagingFactor_s + ";\n";
 
 	string storeTemplate;
@@ -188,7 +184,7 @@ template< typename T > void BeamFormer< T >::generateCode() throw (OpenCLError) 
 		temp = replace(&storeTemplate, "<%NUM%>", *beam_s);
 		stores->append(*temp);
 		delete temp;
-		
+
 		delete beam_s;
 	}
 	this->code = replace(this->code, "<%DEFINITIONS%>", *definitions, true);
--- tuning/BeamFormer.cpp
+++ tuning/BeamFormer.cpp
@@ -1,20 +1,16 @@
+// Copyright 2012 Alessio Sclocco <a.sclocco@vu.nl>
 //
-// Copyright (C) 2012
-// Alessio Sclocco <a.sclocco@vu.nl>
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
 //
-// This program is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
+//     http://www.apache.org/licenses/LICENSE-2.0
 //
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with this program.  If not, see <http://www.gnu.org/licenses/>.
-//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 #include <vector>
 using std::vector;
@@ -75,13 +71,13 @@ int main(int argc, char *argv[]) {
 	} catch ( exception &err ) {
 		cerr << err.what() << endl;
 		return 1;
-	}	
+	}
 
 	vector< cl::Platform > *oclPlatforms = new vector< cl::Platform >();
 	cl::Context *oclContext = new cl::Context();
 	vector< cl::Device > *oclDevices = new vector< cl::Device >();
 	vector< vector< cl::CommandQueue > > *oclQueues = new vector< vector< cl::CommandQueue > >();
-	
+
 	initializeOpenCL(clPlatformID, 1, oclPlatforms, oclContext, oclDevices, oclQueues);
 
 	cout << fixed << endl;
@@ -93,14 +89,14 @@ int main(int argc, char *argv[]) {
 	input->setCLQueue(&(oclQueues->at(clDeviceID)[0]));
 	input->setDeviceReadOnly();
 	input->allocateDeviceData();
-		
+
 	output = new CLData< float >("Output", true);
 	output->allocateHostData(observation.getNrBeams() * observation.getNrChannels() * observation.getNrSamplesPerPaddedSecond() * nrPolarizations * 2);
 	output->setCLContext(oclContext);
 	output->setCLQueue(&(oclQueues->at(clDeviceID)[0]));
 	output->setDeviceWriteOnly();
 	output->allocateDeviceData();
-	
+
 	weights = new CLData< float >("Weights", true);
 	weights->allocateHostData(observation.getNrChannels() * observation.getNrStations() * observation.getNrBeams() * 2);
 	weights->setCLContext(oclContext);
@@ -149,7 +145,7 @@ int main(int argc, char *argv[]) {
 			}
 		}
 	}
-	
+
 	cout << endl;
 	return 0;
 }
