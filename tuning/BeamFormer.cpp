31b90f80563fd3c8727421ff7edb1d1f5ee27f9d
Alessio Sclocco <alessio@sclocco.eu>
2014-11-14 17:00:29 +0100

    Assuming some reuse also for the weights in GB/s computation.

--- tuning/BeamFormer.cpp
+++ tuning/BeamFormer.cpp
@@ -148,7 +148,7 @@ int main(int argc, char * argv[]) {
 
           // Generate kernel
           double gflops = isa::utils::giga((static_cast< long long unsigned int >(observation.getNrBeams()) * observation.getNrChannels() * observation.getNrSamplesPerSecond() * observation.getNrStations() * 16) + (static_cast< long long unsigned int >(observation.getNrBeams()) * observation.getNrChannels() * observation.getNrSamplesPerSecond() * 4));
-          double gbs = isa::utils::giga((static_cast< long long unsigned int >(observation.getNrChannels()) * observation.getNrSamplesPerSecond() * observation.getNrStations() * (observation.getNrBeams() / (beamsPerThread * *beams)) * 4 * sizeof(dataType)) + (static_cast< long long unsigned int >(observation.getNrBeams()) * observation.getNrChannels() * observation.getNrSamplesPerSecond() * 4 * sizeof(dataType)) + (observation.getNrChannels() * observation.getNrStations() * observation.getNrBeams() * 2 * sizeof(float)));
+          double gbs = isa::utils::giga((static_cast< long long unsigned int >(observation.getNrChannels()) * observation.getNrSamplesPerSecond() * observation.getNrStations() * (observation.getNrBeams() / (beamsPerThread * *beams)) * 4 * sizeof(dataType)) + (static_cast< long long unsigned int >(observation.getNrBeams()) * observation.getNrChannels() * observation.getNrSamplesPerSecond() * 4 * sizeof(dataType)) + (observation.getNrChannels() * observation.getNrStations() * (observation.getNrBeams() / (beamsPerThread * *beams)) * 2 * sizeof(float)));
           isa::utils::Timer timer;
           cl::Event event;
           cl::Kernel * kernel;
